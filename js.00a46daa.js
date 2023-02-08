f44e52d546ea92a5d4e30a79ef3ae872e78ec5dd
TarasSirenko <92710593+TarasSirenko@users.noreply.github.com>
2023-02-08 23:07:11 +0000

    Deploying to gh-pages from @ TarasSirenko/goit-js-hw-11-new@214850222cd0f9812c74c895cccce0dac5c968a5 🚀

--- js.00a46daa.js
+++ js.00a46daa.js
@@ -3695,8 +3695,8 @@ let currentLanguage = (0, _forLanguage.getCurrentLanguage)();
 // проверка localStorage на информацию о избранных обектих
 
 let favoritesCardArr = [];
-// localStorage.removeItem('favoritesCard');
 exports.favoritesCardArr = favoritesCardArr;
+localStorage.removeItem('favoritesCard');
 if (localStorage.getItem('favoritesCard')) {
   exports.favoritesCardArr = favoritesCardArr = JSON.parse(localStorage.getItem('favoritesCard'));
 }
@@ -4434,7 +4434,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "3072" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "5899" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
