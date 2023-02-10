6a4276df21a22fa14c75d372c93177cac898ee73
Taras Sirenko <tarassirenko71@gmail.com>
2023-02-11 00:38:07 +0200

    back dist

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -4531,7 +4531,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "4095" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "4912" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
