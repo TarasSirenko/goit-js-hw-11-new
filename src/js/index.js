5d1d48f588b63cccc0288e5031a388143493a545
Taras Sirenko <tarassirenko71@gmail.com>
2023-02-11 16:37:41 +0200

    chenge dist

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-import '../css/styles.css';
-
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import './change-theme.js';
-import './change-language.js';
-import './go-up-btn.js';
-import './add-favorites.js';
-import './home.js';
